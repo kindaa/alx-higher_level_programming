8e5e0c959526ac87d39c43f63e03522e515d383f
Michelle Kinda <resamichel@gmail.com>
2022-06-03 00:56:03 +0300

    1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+
+
 from add_0 import add
-if __name__ == "__main__":
+
+
+def print_add():
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
+if __name__ == '__main__':
+    print_add()
